5850293c9b288de3d2bd87de549902020a25a12b
Joshua Ray <joshua.ray1@snhu.edu>
2020-10-20 18:11:21 -0400

    Made getAmericaData async for cleaner code

--- src/services/covidapi.js
+++ src/services/covidapi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import US from '../assets/countries.json'
+import {US} from '../assets/countries.json'
 
 const proxy = "https://cors-anywhere.herokuapp.com/";        // Proxy for 'Access-Control-Allow-Origin' error
 const EcdcApi = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json";
@@ -11,26 +11,28 @@ const getEuropeData = () => {
     return request.then(response => response.data.records);
 }
 
-const getAmericaData = () => {
+const getAmericaData = async () => {
     const initialRequest = axios.get(CdcApi + "?$select=max(submission_date)");
     const populationRequest = axios.get(CensusApi); // Request to Census API
-    return populationRequest.then(response => {
-       const pops = response.data;
-       return initialRequest.then(response => {
-            const latestDate = new Date(Date.parse(response.data[0].max_submission_date));
-            const dataRequest = axios.get(CdcApi + `?submission_date=${latestDate.getFullYear()}-${latestDate.getMonth()}-${latestDate.getDate()}`);
-            return dataRequest.then(response => {
-                let data = response.data;
-                for (let item of data) {
-                   const fullName = US[item.state];
-                   const popObj = pops.find(i => i[1] === fullName);
-                   if (!popObj)
-                       continue;
-                   item.Population = popObj[0];
-                }
-                return data;
-            });
-       });
+    let pops = [];
+    let latestDate = {};
+
+    await populationRequest.then(response => pops = response.data);
+    pops.shift()
+
+    await initialRequest.then(response => latestDate = new Date(Date.parse(response.data[0].max_submission_date)));
+
+    const dataRequest = axios.get(CdcApi + `?submission_date=${latestDate.getFullYear()}-${latestDate.getMonth() + 1}-${latestDate.getDate()}`);
+    return dataRequest.then(response => {
+        let data = response.data;
+        for (let item of data) {
+            const fullName = US[item.state];
+            const popObj = pops.find(i => i[1] === fullName);
+            if (!popObj)
+                continue;
+            item.Population = popObj[0];
+        }
+        return data;
     });
 }
 
