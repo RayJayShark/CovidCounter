56c6dce3e933ce570b85f21dfd53188a31433e9c
Joshua Ray <joshua.ray1@snhu.edu>
2020-10-20 13:24:41 -0400

    America data now comes with population data from the Census API

--- src/services/covidapi.js
+++ src/services/covidapi.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
+import US from '../assets/countries.json'
 
 const proxy = "https://cors-anywhere.herokuapp.com/";        // Proxy for 'Access-Control-Allow-Origin' error
 const EcdcApi = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json";
 const CdcApi = "https://data.cdc.gov/resource/9mfq-cb36.json";
+const CensusApi = "https://api.census.gov/data/2010/dec/sf1?get=P001001,NAME&for=state";
 
 const getEuropeData = () => {
     const request = axios.get(proxy + EcdcApi);
@@ -11,10 +13,24 @@ const getEuropeData = () => {
 
 const getAmericaData = () => {
     const initialRequest = axios.get(CdcApi + "?$select=max(submission_date)");
-    return initialRequest.then(response => {
-        const latestDate = new Date(Date.parse(response.data[0].max_submission_date));
-        const dataRequest = axios.get(CdcApi + `?submission_date=${latestDate.getFullYear()}-${latestDate.getMonth()}-${latestDate.getDate()}`);
-        return dataRequest.then(response => response.data);
+    const populationRequest = axios.get(CensusApi); // Request to Census API
+    return populationRequest.then(response => {
+       const pops = response.data;
+       return initialRequest.then(response => {
+            const latestDate = new Date(Date.parse(response.data[0].max_submission_date));
+            const dataRequest = axios.get(CdcApi + `?submission_date=${latestDate.getFullYear()}-${latestDate.getMonth()}-${latestDate.getDate()}`);
+            return dataRequest.then(response => {
+                let data = response.data;
+                for (let item of data) {
+                   const fullName = US[item.state];
+                   const popObj = pops.find(i => i[1] === fullName);
+                   if (!popObj)
+                       continue;
+                   item.Population = popObj[0];
+                }
+                return data;
+            });
+       });
     });
 }
 
