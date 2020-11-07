65e5b938e21b1f1a541234e307f21b133fe8103c
Joshua Ray <superdog1695@yahoo.com>
2020-11-07 16:54:29 -0500

    Replaced states and effects with queries

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { useQuery } from 'react-query'
+import { ReactQueryDevtools } from 'react-query-devtools'
 import CounterTable from "./components/CounterTable";
 import Timestamp from "./components/Timestamp";
 import Territory from "./components/Territory";
 import covidService from "./services/covidService";
 
 function App() {
-    const [ europeData, setEuropeData ] = useState([]);
-    const [ americaData, setAmericaData ] = useState([]);
 
-    useEffect(() => {
-        covidService.getEuropeData().then(data => setEuropeData(data));
-        covidService.getAmericaData().then(data => setAmericaData(data));
-    }, []);
+    const { data: europeData } = useQuery('europe', covidService.getEuropeData);
+    const { data: americaData } = useQuery('america', covidService.getAmericaData);
 
     return (
     <div>
         <BrowserRouter>
             <Switch>
                 <Route path='/:territory'>
-                    <Territory covidData={[...americaData, ...europeData]} />
+                    <Territory covidData={[...americaData ?? [], ...europeData ?? []]} />
                 </Route>
                 <Route path="/">
-                    <CounterTable europeData={europeData} americaData={americaData} />
+                    <CounterTable europeData={europeData ?? []} americaData={americaData ?? []} />
                 </Route>
             </Switch>
         </BrowserRouter>
         <Timestamp />
+        <ReactQueryDevtools initialIsOpen />
     </div>
   );
 }
